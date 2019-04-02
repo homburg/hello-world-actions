3fff98c99fb1ed34435274cb448a5fdd682b4b5b
Thomas B Homburg <thomas@madethis.dk>
2019-04-02 23:02:11 +0200

    Fix revision

--- github-action/index.js
+++ github-action/index.js
@@ -3,10 +3,10 @@ const { Toolkit } = require("actions-toolkit");
 // Run your GitHub Action!
 Toolkit.run(async tools => {
   tools.log.info((await tools.runInWorkspace("ls", ["-la"])).stdout);
-  tools.log((await tools.runInWorkspace("git", ["log", "--stat"])).stdout);
   tools.log(
-    (await tools.runInWorkspace("git", ["diff", "--name-only", "HEAD^3"]))
+    (await tools.runInWorkspace("git", ["diff", "--name-only", "HEAD~3"]))
       .stdout
   );
+  tools.log((await tools.runInWorkspace("git", ["log", "--stat"])).stdout);
   tools.exit.success("We did it!");
 });
