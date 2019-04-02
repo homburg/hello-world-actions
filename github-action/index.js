23767c8878e8ee53dd8ad2031b6829e670e735b0
Thomas B Homburg <thomas@madethis.dk>
2019-04-02 22:43:20 +0200

    Test

--- github-action/index.js
+++ github-action/index.js
@@ -2,8 +2,8 @@ const { Toolkit } = require('actions-toolkit')
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  tools.log(await tools.runInWorspace("ls", ["-la"]));
-  const result await tools.runInWorkspace("git", ["log", "--stat"]);
+  tools.log(await tools.runInWorkspace("ls", ["-la"]));
+  const result = await tools.runInWorkspace("git", ["log", "--stat"]);
   tools.log(result);
   tools.exit.success('We did it!')
 })
--- github-action/index.test.js
+++ github-action/index.test.js
@@ -1,23 +1,31 @@
-const { Toolkit } = require('actions-toolkit')
+const { Toolkit } = require("actions-toolkit");
 
-describe('github-action', () => {
-  let action, tools
+describe("github-action", () => {
+  let action, tools;
 
   // Mock Toolkit.run to define `action` so we can call it
-  Toolkit.run = jest.fn((actionFn) => { action = actionFn })
+  Toolkit.run = jest.fn(actionFn => {
+    action = actionFn;
+  });
   // Load up our entrypoint file
-  require('.')
+  require(".");
 
   beforeEach(() => {
     // Create a new Toolkit instance
-    tools = new Toolkit()
+    tools = new Toolkit();
     // Mock methods on it!
-    tools.exit.success = jest.fn()
-  })
+    tools.exit.success = jest.fn();
+    tools.runInWorkspace = jest.fn();
+    tools.log = jest.fn();
+  });
 
-  it('exits successfully', () => {
-    action(tools)
-    expect(tools.exit.success).toHaveBeenCalled()
-    expect(tools.exit.success).toHaveBeenCalledWith('We did it!')
-  })
-})
+  it("exits successfully", async () => {
+    action(tools);
+    for (var i = 0; i < 2; i++) {
+      await expect(tools.runInWorkspace).toHaveBeenCalled();
+      expect(tools.log).toHaveBeenCalled();
+    }
+    expect(tools.exit.success).toHaveBeenCalled();
+    expect(tools.exit.success).toHaveBeenCalledWith("We did it!");
+  });
+});
