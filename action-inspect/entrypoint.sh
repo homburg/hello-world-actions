75990d8c464b5999c63784eb12adcc049cb43231
Thomas B Homburg <thomas@madethis.dk>
2019-04-07 15:41:27 +0200

    Git user config

--- action-inspect/entrypoint.sh
+++ action-inspect/entrypoint.sh
@@ -10,6 +10,9 @@ echo date +%Y%m%d-%H%M%S >> output.txt
 
 git add output.txt
 
+git config --global user.email "action-inspect@localhost"
+git config --global user.name "action-inspect"
+
 git status
 
 git commit -m "ci commit"
